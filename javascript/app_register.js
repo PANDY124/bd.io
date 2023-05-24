32e7ad1693abe7b20ed6e0eeee137f09ee63a3f4
PANDY124 <conejo3444@gmail.com>
2023-05-23 23:57:03 -0600

    termino de regitro de usuarios con comentarios

--- javascript/app_register.js
+++ javascript/app_register.js
@@ -1,7 +1,9 @@
+//llamamos la funcion del onclick y con la funcion async para poder recoger e enviar datos de php a js y
+//viceversa
 const validarg=async()=>{
     var user_rg=document.querySelector('#user').value;
     var pass_rg=document.querySelector('#pass').value;
-
+//preguntamos que los campos no esten vacios
     if(user_rg.trim()==='' || pass_rg.trim()===''){
         Swal.fire({
             icon: 'error',
@@ -12,7 +14,7 @@ const validarg=async()=>{
         })
 
 }
-
+//parte para insertar los datos a mysql
 const datos =  new FormData();
 datos.append('user',user_rg);
 datos.append('pass',pass_rg);
@@ -20,9 +22,9 @@ var respuesta=await fetch("php/funciones.php",{
     method:'POST',
     body:datos
 });
-
+//recojemos los resultados de las consultas en php
 var resultado = await respuesta.json();
-
+//preguntamos si el resultado que arroja php es success o false
 if(resultado.success=="true"){
     Swal.fire({
         icon: 'success',
